b4c43d8d8f894267de74b601850ab752f48ee95e
reshmak726 <reshmak726@eng.rizvi.edu>
2021-09-06 20:52:06 +0530

    subarray of given sum code added

---
+++ subarray_givensum.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+void findsubarray(int a[],int n,int target){
+    unordered_map<int,int> map;
+    map.insert(pair<int,int>(0,-1));
+    int sumsofar = 0;
+    for(int i=0;i<n;i++){
+        sumsofar+=a[i];
+        if(map.find(sumsofar-target)!=map.end()){
+            cout<< "subarray found [" <<map[sumsofar-target]+1 << " , "  << i << "]" <<endl;
+            return;
+        }
+        map.insert(pair<int ,int >(sumsofar,i));
+    }
+}
+int main(){
+    int a[] = {2,6,1,5,4,7};
+    int target = 10;
+    int n = sizeof(a)/sizeof(a[0]);
+    findsubarray(a,n,target);
+    return 0;
+}
