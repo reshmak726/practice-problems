90c0abc78134124976bc956c9a856ff918fefd2e
reshmak726 <reshmak726@eng.rizvi.edu>
2021-12-06 20:21:53 +0530

    added basic codes on list

---
+++ .vscode/basic python/18.py
@@ -0,0 +1,19 @@
+# l=[]
+# n=int(input("enter the elements: "))
+# for i in range (n,n+1):
+#     ele = int(input("enter the numbers: "))
+#     l.append(ele)
+
+# # for n in l:
+# #     if n % 2==0:
+# #         print(n,end=" ")
+# even_nos=[num for num in n if num % 2==0]
+# print("even numbers in the list",even_nos)
+
+
+# l=[12,3,4,6,5,45,44]
+inputstring=input("enter the list elements: ")
+list=inputstring.split()
+for num in list:
+    if num % 2 == 0:
+        print(num,end=" ")
---
+++ .vscode/basic python/19.py
@@ -0,0 +1,3 @@
+l=[1,3,89,66,65]
+odd=[num for num in l if num%2==1]
+print("the odd numbers are: ",odd)
