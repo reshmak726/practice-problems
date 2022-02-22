8bcfe1954baf8db538adcbca8a539f1710d50b8d
Reshma Khan <reshmak726@gmail.com>
2022-02-22 12:41:27 +0530

    detecting armstrong number

---
+++ practice8.py
@@ -0,0 +1,17 @@
+from tkinter import N
+
+
+n=int(input("enter the number "))
+sum=0
+order=len(str(n))
+copy_n=n
+while(n>0):
+    digit=n%10
+    sum+=digit**order
+    n=n//10
+
+if(sum==copy_n):
+    print(f"{copy_n} is an armstrong number")
+else:
+    print(f"{copy_n} is not an armstrong number")
+ 
